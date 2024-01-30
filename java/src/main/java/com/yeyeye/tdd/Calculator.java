9a80da0a85ccecc5e88c6688e2ee6d76a934e92b
wchen <wchen137@qq.com>
2024-01-30 17:18:19 +0800

    refactor: 重构业务逻辑

--- java/src/main/java/com/yeyeye/tdd/Calculator.java
+++ java/src/main/java/com/yeyeye/tdd/Calculator.java
@@ -6,15 +6,27 @@ import org.springframework.stereotype.Component;
 public class Calculator {
     public int calculate(int input) {
         if (input > 0) {
-            if (input < 100) {
-                return input * input;
-            } else {
-                return input - 1;
-            }
+            return doGreaterThan0(input);
         } else if (input < 0) {
-            return input + 1;
+            return doLowerThan0(input);
         } else {
-            return 0;
+            return doEqualTo0(input);
         }
+    }
+
+    private int doGreaterThan0(int input) {
+        if (input < 100) {
+            return input * input;
+        } else {
+            return input - 1;
+        }
+    }
+
+    private int doEqualTo0(int input) {
+        return input;
+    }
+
+    private int doLowerThan0(int input) {
+        return input + 1;
     }
 }
